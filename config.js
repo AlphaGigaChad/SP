95e4960adf981af18e6811016c9e32b93b06923a
AlphaGigaChad <106097482+AlphaGigaChad@users.noreply.github.com>
2022-06-14 18:41:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ const CONFIG = {
 
 	// Weather
 	weatherKey: 'f028371db03f057bb2bf95d8618412a3', // Write here your API Key
-	weatherIcons: 'Onedark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherIcons: 'OneDark', // 'OneDark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
